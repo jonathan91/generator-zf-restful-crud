92930f9e7417bf6b32fe81e761144990804f6018
Jonathan <jonathan.monteiro6@gmail.com>
2018-12-03 11:04:34 -0200

    Update AppAbstractController
    Update AppAbstractController

--- app/templates/backend/skeleton/module/Application/src/Controller/AppAbstractController.php
+++ app/templates/backend/skeleton/module/Application/src/Controller/AppAbstractController.php
@@ -12,9 +12,24 @@ abstract class AppAbstractController extends AbstractRestfulController
     		$this->entityManager = $entityManager;
   	}
 	
+	public function setEventManager(EventManagerInterface $events)
+    	{
+        	parent::setEventManager($events);
+        	$events->attach('dispatch', array($this, 'isAuthorized'), 10);
+	}
+	
 	public function getServiceManager()
 	{
 		return $this->entityManager;
+	}
+    	
+	protected function isAuthorized()
+	{
+		$request = $event->getRequest();
+		$response = $event->getResponse();
+		$isAuthorizationRequired = $event->getRouteMatch()->getParam('authorizationIsRequired');
+		$config = $event->getApplication()->getServiceManager()->get('Config');
+		//require security module to validate jwt
 	}
 }
 
