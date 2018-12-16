49cde753e1859581759cc5855e3c91b6081578a5
Jonathan Monteiro <jonathan.monteiro6@gmail.com>
2018-12-16 17:34:21 -0200

    Ajustar itens para geração de codigo

--- app/templates/backend/skeleton/module/Application/src/Controller/AppAbstractController.php
+++ app/templates/backend/skeleton/module/Application/src/Controller/AppAbstractController.php
@@ -15,24 +15,9 @@ abstract class AppAbstractController extends AbstractRestfulController
 		$this->serviceManager = $serviceManager;
 	}
 
-	public function setEventManager(EventManagerInterface $events)
-  	{
-        	parent::setEventManager($events);
-        	$events->attach('dispatch', array($this, 'isAuthorized'), 10);
-	}
-
 	public function getServiceManager()
 	{
 		return $this->entityManager;
-	}
-
-	protected function isAuthorized()
-	{
-		$request = $event->getRequest();
-		$response = $event->getResponse();
-		$isAuthorizationRequired = $event->getRouteMatch()->getParam('authorizationIsRequired');
-		$config = $event->getApplication()->getServiceManager()->get('Config');
-		//require security module to validate jwt
 	}
 
 	public function getList()
